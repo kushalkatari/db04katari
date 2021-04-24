003b8841b5736b4cfb84793588db830af689a185
kushalkatari <35985697+kushalkatari@users.noreply.github.com>
2021-04-23 22:15:24 -0500

    part5

--- models/dress.js
+++ models/dress.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose")
 const dressSchema = mongoose.Schema({
 name: String,
-cost: Number,
+cost: {
+    type: Number,
+
+    min:[10,"Min"],
+    max:[1000,"Max"]
+},
 brand: String
 })
 module.exports = mongoose.model("Dress", dressSchema)
